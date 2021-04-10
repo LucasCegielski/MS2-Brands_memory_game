64c1a860d23215170f85a9eec31d357ab38cc4d4
LucasCegielski <lucas.cegielski@yahoo.ie>
2021-04-10 10:40:13 +0000

    added function moves counter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,3 +24,52 @@ function shuffle(array) {
     }
     return array;
 }
+let openCards = [];
+let matchedCards = [];
+let matchedCount = 0;
+let moveCount = 0;
+let matchingCards;
+let displayedCards;
+
+const allCards = document.getElementsByClassName('card');
+const successModal = document.getElementById('success-modal');
+const closeModalButton = document.getElementsByClassName('close-button')[0];
+const playAgainButton = document.getElementById('button-restart');
+const restartButton = document.getElementsByClassName("restart")[0];
+
+restartButton.addEventListener('click', restartGame);
+document.body.addEventListener('click', function (e) {
+    if (e.target.classList.contains("not-shown") && (document.querySelectorAll('.show').length < 2)) {
+        if (document.querySelectorAll('.not-shown').length == 16 && moveCount === 0) {
+            runTimer();
+        }
+        let classMatch = e.target.querySelector('i').className;
+        if (openCards.length < 1) {
+            revealCard(e);
+            addOpenCard(e);
+        } else {
+            if (openCards.includes(classMatch)) {
+                matchedCards.push(classMatch);
+                revealCard(e);
+                matchCards();
+                updateMoveCount();
+                openCards = [];
+            } else {
+                revealCard(e);
+                hideCards();
+            }
+        }
+    } else {
+        console.log("clicked outside cards");
+    }
+});
+
+function updateMoveCount() {
+    moveCount += 1;
+    if (moveCount === 1) {
+        document.getElementById("moves-made").innerHTML = moveCount + " Move";
+    } else {
+        document.getElementById("moves-made").innerHTML = moveCount + " Moves";
+    }
+    adjustStars();
+}
